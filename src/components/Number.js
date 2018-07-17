1a0d8dbc0129c1df3ce9a964549348c61bb3a3ab
Mike Cousins <mike@mikecousins.com>
2018-07-17 17:00:35 -0600

    Adding display units

---
+++ src/components/Number.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Number = (props) => {
+    const { value } = props;
+    const trillion = 1000000000000;
+    const billion = 1000000000;
+    const million = 1000000;
+    if (value / trillion > 1) {
+        return <React.Fragment>{`${value / trillion}T`}</React.Fragment>;
+    } else if (value / billion > 1) {
+        return <React.Fragment>{`${value / billion}B`}</React.Fragment>;
+    } else if (value / million > 1) {
+        return <React.Fragment>{`${value / million}M`}</React.Fragment>;
+    } else {
+        return <React.Fragment>{value}</React.Fragment>;
+    }
+}
+
+Number.propTypes = {
+    value: PropTypes.number.isRequired,
+};
+
+export default Number;
