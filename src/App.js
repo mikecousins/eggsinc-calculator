191d9bbb329ae2d9c7d7d818add1a27ad4c42e87
Mike Cousins <mike@mikecousins.com>
2018-07-17 17:22:05 -0600

    Starting work on adding input units

--- src/App.js
+++ src/App.js
@@ -35,8 +35,16 @@ class App extends Component {
               </div>
             </div>
             <div className="mb-4 mt-4">
-              <label className="block text-grey-darker text-sm font-bold mb-2">Current Chickens</label>
-              <input className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight" type="text" value={this.state.currentChickens} onChange={e => this.setState({ currentChickens: e.target.value })} />
+              <label className="block text-grey-darker text-sm font-bold mb-2 flex flex-wrap items-stretch w-full relative">Current Chickens</label>
+              <div className="flex flex-wrap items-stretch w-full mb-4 relative">
+                <input className="shadow  text-grey-darker flex-shrink flex-grow flex-auto leading-normal w-px flex-1 border h-10 border-grey-light rounded rounded-l-none px-3 relative" type="text" value={this.state.currentChickens} onChange={e => this.setState({ currentChickens: e.target.value })} />
+                <div class="flex -mr-px">
+                  <select class="flex items-center leading-normal bg-grey-lighter rounded rounded-l-none border border-l-0 border-grey-light px-3 whitespace-no-wrap text-grey-dark text-sm">
+                    <option>B</option>
+                    <option>M</option>
+                </select>
+                </div>
+              </div>
             </div>
             <div className="mb-4">
               <label className="block text-grey-darker text-sm font-bold mb-2">Current Eggs</label>
